0adfddc13c6d637f61454c560f3847c936f2fcc2
Caio Peres <caiofperes@hotmail.com>
2023-01-20 20:26:45 -0300

    Files added

---
+++ CPPThreadClass/Window.h
@@ -0,0 +1,28 @@
+#include <windows.h>
+
+
+template<class T> class Window {
+
+public:
+
+    Window() {
+        SetWindowLongPtrA(hwnd, 0, (LONG_PTR)this);
+    }
+
+    static LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+    {
+        Simple* me = reinterpret_cast<Simple*>(GetWindowLongPtr(hwnd, GWLP_USERDATA));
+        if (me) return me->realWndProc(hwnd, msg, wParam, lParam);
+        return DefWindowProc(hwnd, msg, wParam, lParam);
+    }
+
+private:
+
+    HWND hwnd;
+
+    LRESULT CALLBACK realWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+    {
+        // member function!
+    }
+
+};
---
+++ CPPThreadClass/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include "Thread.h"
+
+class HelloWorld
+{
+public:
+    DWORD print()
+    {
+        std::cout << "Hello World!" << std::endl;
+        return 0;
+    }
+};
+
+int main(void)
+{
+    // Random object with DWORD method (void)
+    HelloWorld world;
+    // thread should call print method of world.
+
+    Thread<HelloWorld> thread(&world, &HelloWorld::print);
+    if (thread.start())
+        std::cout << "Thread start()" << std::endl;
+    thread.join(); // wait for thread
+
+    return 0;
+}
